6a43544f6b0f459928dac85e563506c371e5760b
Sandeep Narayana <sandeep.narayana2013@outlook.com>
2026-02-23 20:26:45 +0530

    update in 8 file

--- 8-using_single_quote.sh
+++ 8-using_single_quote.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 NAME="DEVOPS"
+SKILLS="Skill Updated"
 
 # with single quote
-echo 'Hello $NAME'
+echo 'Hello $NAME , $SKILLS'
 
 # with double quotes
-echo "Hello $NAME"
+echo "Hello $NAME , $SKILLS"
 
 #without quotes
-echo Hello $NAME
+echo Hello $NAME, $SKILLS
