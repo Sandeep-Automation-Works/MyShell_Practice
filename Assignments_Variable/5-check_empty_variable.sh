5a8317281bac022fcf3f31446823305474d9d273
Sandeep Narayana <sandeep.narayana2013@outlook.com>
2026-02-23 22:04:17 +0530

    updated comments

--- Assignments_Variable/5-check_empty_variable.sh
+++ Assignments_Variable/5-check_empty_variable.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+
+# Start of the comment
+: <<'COMMENT'
+Assignment 5 – Check Empty Variable
+Write a script that:
+Takes USERNAME variable
+If empty → print "Username is required"
+Else → print "Welcome <username>"
+Use proper quoting.
+COMMENT
+#END OF THE COMMENT
+
 USERNAME="ec2-user"
 
 if [ -z $USERNAME ]; then
