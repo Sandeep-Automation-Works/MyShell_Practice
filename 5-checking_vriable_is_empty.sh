da14c05ddb663dde55f575a8e557a38ae9636275
Sandeep Narayana <sandeep.narayana2013@outlook.com>
2026-02-23 20:19:44 +0530

    6 file created

--- 5-checking_vriable_is_empty.sh
+++ 5-checking_vriable_is_empty.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+# -z It checks whether a string is empty
 if [ -z "$USERNAME" ]; then
     echo "Username not provided"
 fi
---
+++ 6logging_script_name.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+SCRIPT_NAME="$0"
+echo "Running script: $SCRIPT_NAME"
