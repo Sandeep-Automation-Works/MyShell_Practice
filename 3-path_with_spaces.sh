284845b8f4c3a9817c1241aeb4809fdfee67788d
Sandeep Narayana <sandeep.narayana2013@outlook.com>
2026-02-23 20:08:37 +0530

    modified 3 script file

--- 3-path_with_spaces.sh
+++ 3-path_with_spaces.sh
@@ -2,7 +2,7 @@
 
 FOLDER="/tmp/devops/My Folder"
 FILE="/tmp/devops/My Folder/file.txt"
-mkdir "$FOLDER"
+mkdir -p "$FOLDER"
 touch "$FILE"
 echo "This is your first expirentment of writtent sample script" > $FILE
 cat "$FILE"
