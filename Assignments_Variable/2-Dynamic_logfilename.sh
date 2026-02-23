504a7819e2e35f414c5d815614c465c3622872c0
Sandeep Narayana <sandeep.narayana2013@outlook.com>
2026-02-23 21:40:43 +0530

    modified

--- Assignments_Variable/2-Dynamic_logfilename.sh
+++ Assignments_Variable/2-Dynamic_logfilename.sh
@@ -2,9 +2,10 @@
 
 TODAY="$(date +%Y-%m-%d)"
 LOG_PATH="/tmp/temp/"
-LOG_FILE="/tmp/temp/backup-$TODAY.log"
+LOG_FILE="backup-$TODAY.log"
 mkdir -p "$LOG_PATH"
-touch /$LOG_PATH/backup-"$TODAY".log
+echo "This is file log file created today:" > "/$LOG_PATH/$LOG_FILE"
+#touch /$LOG_PATH/backup-"$TODAY".log
 
 echo "today created log filename: $LOG_FILE"
 
