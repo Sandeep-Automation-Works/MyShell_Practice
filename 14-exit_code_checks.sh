9d90a1235c863bc122f5e690657807263c5cef9a
Sandeep Narayana <sandeep.narayana2013@outlook.com>
2026-02-24 07:09:14 +0530

    modified

--- 14-exit_code_checks.sh
+++ 14-exit_code_checks.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 TEST_PATH="/var/log/test"
-if mkdir "TEST_PATH"; then
+if mkdir "$TEST_PATH"; then
     echo "Directory created successfully"
 else
     echo "Failed to create directory"
+    echo "Directory was not created at this location $TEST_PATH"
 fi
 
-echo "Directory created location $TEST_PATH"
